c8ce52e01fdf9cf6d2cb53ebcf45c53520b8c0ec
Shambhuraj Patil <shambhurajpatil11@gmail.com>
2023-01-22 10:01:50 +0530

    updated all

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 
 from keras.models import load_model
@@ -10,27 +9,28 @@ from keras.utils import pad_sequences
 
 app = Flask(__name__)
 
-model=load_model('weights.02-0.17.h5')
+
+model=load_model('weights.02-0.19.h5')
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=request.form.get('text')
+    data=request.form.get('data')
    #print(data)
     corpus=[]
     lema=WordNetLemmatizer()
-    for i in range(len(data)):
-            review=re.sub('[^a-zA-Z]',' ',data)
-            review=review.lower()
-            review=review.split()
-            review=[lema.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
-            review=' '.join(review)
-            corpus.append(review)
-        
+    review=re.sub('[^a-zA-Z]',' ',data)
+    review=review.lower()
+    review=review.split()
+    review=[lema.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
+    review=' '.join(review)
+    corpus.append(review)
+
     voc_size=15000
     one_hot_rep=[one_hot(word,voc_size)for word in corpus]
     sent_length=20
@@ -40,5 +40,6 @@ def predict():
     return render_template('index.html',prediction_text="This news seems to be fake with probablity of {}".format(output))
 
 
-if __name__=='main':
+if __name__ == '__main__':
     app.run(debug=True)
+
--- text_prepo.py
+++ text_prepo.py
@@ -1,4 +1,4 @@
-"""import nltk
+#import nltk
 import re
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -15,16 +15,14 @@ class text_prepro:
         self.one_hot_rep=[]
         self.embed_doc=[]
         lema=WordNetLemmatizer()
-       
-        for i in range(len(self.data)):
-            self.review=re.sub('[^a-zA-Z]',' ',self.data)
-            self.review=self.review.lower()
-            self.review=self.review.split()
-            self.review=[lema.lemmatize(word) for word in self.review if not word in set(stopwords.words('english'))]
-            self.review=' '.join(self.review)
+
+        self.review=re.sub('[^a-zA-Z]',' ',self.data)
+        self.review=self.review.lower()
+        self.review=self.review.split()
+        self.review=[lema.lemmatize(word) for word in self.review if not word in set(stopwords.words('english'))]
+        self.review=' '.join(self.review)
         voc_size=15000
         self.one_hot_rep=[one_hot(word,voc_size)for word in self.review]
         sent_length=20
         self.embed_doc=pad_sequences(self.one_hot_rep,padding='pre',maxlen=sent_length)
         return self.embed_doc
-"""
